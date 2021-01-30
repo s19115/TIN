69943c3315de2f6857663403f64dd9f2b2f60477
s19115 <s19115@pjwstk.edu.pl>
2021-01-30 16:06:24 +0100

    mp1

--- tin-lootdrop-ignut-s19115/config/sequelize/sequelize.js
+++
@@ -1,8 +0,0 @@
-const Sequelize = require('sequelize');
-
-const sequelize = new Sequelize('tin-example-sequelize', 'root', 'root', {
-    dialect: 'mysql',
-    host: 'localhost'
-});
-
-module.exports = sequelize;
